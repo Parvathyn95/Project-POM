2be0992c6617561633226786dce265a5164b857f
Parvathyn95 <parvathycet95@gmail.com>
2022-12-22 22:10:47 +0400

    Alert,popup,iframe

--- ProjectPOM/src/main/java/pages/RowAndColumnCount.java
+++ ProjectPOM/src/main/java/pages/RowAndColumnCount.java
@@ -12,13 +12,15 @@ import utilities.ExcelUtility;
 
 public class RowAndColumnCount {
 	public WebDriver driver;
+	By table = By.xpath("//table[@id='dtBasicExample']");
 	By columnElementsList = By.xpath("//table[@id='dtBasicExample']//child::th[@class='th-sm sorting_disabled']");
 	By rowElementsList = By.xpath("//table[@id='dtBasicExample']//child::tbody//tr");
 	public RowAndColumnCount(WebDriver driver) {
 		this.driver=driver;
 	}
 		public void countColumnSize() {
-			List <WebElement> columnElement = driver.findElements(columnElementsList);
+			WebElement Table = driver.findElement(table);
+			List <WebElement> columnElement = Table.findElements(columnElementsList);
 			int columnCount=columnElement.size();
 			for(WebElement column: columnElement) {
 				System.out.println(column.getText());
@@ -28,7 +30,8 @@ public class RowAndColumnCount {
 			String expectedField;
 			ExcelUtility excelUtilityObj = new ExcelUtility();
 			expectedField = excelUtilityObj.getStringMultiColor(1, 0, "\\src\\main\\java\\Resources\\color.xlsx", "RowColumnTable");
-			List  <WebElement> rowElement= driver.findElements(rowElementsList);
+			WebElement Table = driver.findElement(table);
+			List  <WebElement> rowElement= Table.findElements(rowElementsList);
 			int rowCount=rowElement.size();
 			for(WebElement row : rowElement) {
 				if(row.getText().equals(expectedField)) {
